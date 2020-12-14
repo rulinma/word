b6a6a1cf62d3f6d7d557bfa29927b7777a2a2425
rulinma <rulinma@gmail.com>
2020-12-14 22:13:49 +0800

    imageview

--- word/word/rulinma/controller/HomeViewController.swift
+++ word/word/rulinma/controller/HomeViewController.swift
@@ -23,19 +23,18 @@ class HomeViewController: UIViewController {
         // add subview to show image
         let imageName = "Splash.png"
         let image = UIImage(named: imageName)
-        let  imageView = UIImageView(image: image!)
+        let imageView = UIImageView(image: image!)
         imageView.frame = CGRect(x: 100, y: 100, width: 200, height: 200)
         
         let imgClick = UITapGestureRecognizer(target: self, action: #selector(self.imageTapped));
         imageView.addGestureRecognizer(imgClick);
         imageView.isUserInteractionEnabled = true
-        
+
         view.addSubview(imageView)
         
         // imageView 添加点击事件
-        
+
         // 显示另外的subview
-        
     }
     
     // 点击事件方法
@@ -52,8 +51,41 @@ class HomeViewController: UIViewController {
         // add new view
         // subview
         // click show imageView
+   
+        // view.backgroundColor = UIColor.yellowColor()
+        let uiLable = UILabel(frame: CGRect(x: 100, y:100, width: 200, height: 200))
+        uiLable.textAlignment = NSTextAlignment.center;
+        uiLable.text = "word"
+        uiLable.backgroundColor = UIColor.blue
+        
+        
+        let labelClick = UITapGestureRecognizer(target: self, action: #selector(self.labelTapped));
+        
+        uiLable.addGestureRecognizer(labelClick);
+        uiLable.isUserInteractionEnabled = true
+        view.addSubview(uiLable)
+        
     }
     
+    
+    // 点击事件方法
+    @objc func labelTapped(sender: UITapGestureRecognizer) -> Void {
+        NSLog("labelTapped")
+        
+        let im = sender.view as? UILabel
+        im?.isHidden = true;
+    
+        let imageName = "Splash.png"
+        let image = UIImage(named: imageName)
+        let imageView = UIImageView(image: image!)
+        imageView.frame = CGRect(x: 100, y: 100, width: 200, height: 200)
+        
+        let imgClick = UITapGestureRecognizer(target: self, action: #selector(self.imageTapped));
+        imageView.addGestureRecognizer(imgClick);
+        imageView.isUserInteractionEnabled = true
+        
+        view.addSubview(imageView)
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
